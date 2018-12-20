6949429a62e9dfbfd67b6cc0eed20e0c142c3c9d
Dmitry Syusin <sitric.grp@gmail.com>
2018-12-20 20:33:16 +0700

    quick push

---
+++ ChildTwo/src/main/java/com/sitric/Solution.java
@@ -0,0 +1,10 @@
+package com.sitric;
+
+import com.sitric.Person;
+
+public class Solution {
+    public static void main(String[] args) {
+        Person person = new Person("Johan", 25, 180);
+        System.out.println(person.toString());
+    }
+}
--- src/main/java/com/sitric/Solution.java
+++ src/main/java/com/sitric/Solution.java
@@ -1,7 +1,11 @@
 package com.sitric;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+
 public class Solution {
     public static void main(String[] args) {
+        Objects.hash(new BigDecimal(10.1));
         System.out.println("Hi from parent module");
     }
 }
